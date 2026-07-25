fd787c4de64297f20263e43be70a02ade2640522
Aryen Mukundam <aryen.24bcs10198@sst.scaler.com>
2026-07-25 10:54:48 +0530

    Time: 1 ms (100%), Space: 42.3 MB (97.55%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxProduct(int n) {
+
+        int max = 0, secMax = 0;
+        while(n > 0){
+
+            int rem = n % 10;
+            if(rem >= max){
+                secMax = max;
+
+                max = rem;
+            }
+
+            else if(rem < max && rem >= secMax){
+                secMax = rem;
+            }
+            
+
+            n /= 10;
+        }
+
+
+        return max * secMax;
+
+
+        
+    }
+}
