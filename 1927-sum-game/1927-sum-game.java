0dd7a5c8368d2d86227c8fe5f60969ee2acc781e
Aryen Mukundam <aryen.24bcs10198@sst.scaler.com>
2026-08-24 01:07:34 +0530

    Time: 8 ms (53.24%), Space: 47.1 MB (26.62%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -3,22 +3,22 @@ class Solution {
         int n = num.length();
 
         int leftSum = 0, rightSum = 0;
-        int leftQ = 0, rightQ = 0;
+        int leftQues = 0, rightQues = 0;
 
         for (int i = 0; i < n / 2; i++) {
             if (num.charAt(i) == '?')
-                leftQ++;
+                leftQues++;
             else
                 leftSum += num.charAt(i) - '0';
         }
 
         for (int i = n / 2; i < n; i++) {
             if (num.charAt(i) == '?')
-                rightQ++;
+                rightQues++;
             else
                 rightSum += num.charAt(i) - '0';
         }
 
-        return (leftSum - rightSum) * 2 != (rightQ - leftQ) * 9;
+        return (leftSum - rightSum) != (rightQues - leftQues) * 4.5;
     }
 }
