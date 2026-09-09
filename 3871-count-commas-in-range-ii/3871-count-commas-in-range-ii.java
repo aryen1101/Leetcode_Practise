f726f6ef102bf749f3862133e7a3401b11eddf29
Aryen Mukundam <aryen.24bcs10198@sst.scaler.com>
2026-09-09 12:33:58 +0530

    Time: 1 ms (99.36%), Space: 42.3 MB (93.59%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,12 +1,28 @@
 class Solution {
     public long countCommas(long n) {
-        
-        if(n < 1000) return 0;
-        if(n >= 1000 && n < 1000000) return n - 999;
-        if(n >= 1000000 && n < 1000000000) return (2 * (n - 999999)) + countCommas(999999);
-        if(n >= 1000000000L && n < 1000000000000L) return (3 * (n - 999999999L)) + countCommas(999999999L);
-        if(n >= 1000000000000L && n < 1000000000000000L) return (4 * (n - 999999999999L)) + countCommas(999999999999L);
 
-        return (5 * (n - 999999999999999L)) + countCommas(999999999999999L);
+        long ans = 0;
+
+        if (n >= 1000) {
+            ans += n - 999;
+        }
+
+        if (n >= 1000000) {
+            ans += n - 999999;
+        }
+
+        if (n >= 1000000000L) {
+            ans += n - 999999999L;
+        }
+
+        if (n >= 1000000000000L) {
+            ans += n - 999999999999L;
+        }
+
+        if (n >= 1000000000000000L) {
+            ans += n - 999999999999999L; 
+        }
+
+        return ans;
     }
 }
