0c6bfc6f6010a2ceffa57006971e1baa07e17d90
Aryen Mukundam <aryen.24bcs10198@sst.scaler.com>
2026-09-26 14:53:11 +0530

    Time: 35 ms (72.4%), Space: 90.8 MB (78.73%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -1,38 +1,35 @@
 class Solution {
     public String evaluate(String s, List<List<String>> knowledge) {
-
-        HashMap<String, String> map = new HashMap<>();
-        for (List<String> list : knowledge) {
+        
+        int n = s.length();
+        HashMap<String , String> map = new HashMap<>();
+        for(List<String> list : knowledge){
             map.put(list.get(0), list.get(1));
         }
+
+        int start = -1;
         StringBuilder sb = new StringBuilder();
-        int start = -1, end = s.length();
-        for (int i = 0; i < s.length(); i++) {
+        for(int i = 0 ; i < n ; i++){
 
-            if (s.charAt(i) == '(') {
+            if(s.charAt(i) == '('){
                 start = i;
-            } else if (s.charAt(i) == ')') {
-                end = i;
             }
 
-            else if (start == -1 && end == s.length()) {
-                sb.append(s.charAt(i));
-            }
+            else if(s.charAt(i) == ')'){
 
-            if(start != -1 && end != s.length()){
-                if (map.containsKey(s.substring(start+1, end))) {
-                    sb.append(map.get(s.substring(start+1, end)));
-                } else {
+                String ss = s.substring(start+1, i);
+                if(map.containsKey(ss)){
+                    sb.append(map.get(ss));
+                }
+                else{
                     sb.append('?');
                 }
-            }
-
-            if(end != s.length()) {
                 start = -1;
-                end = s.length();
+            }
+            else if(start == -1){
+                sb.append(s.charAt(i));
             }
         }
         return sb.toString();
-
     }
 }
